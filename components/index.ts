d5fe153de6b6acefb8fdff4cfba570ac971572e7
Swagat Talukdar <swagattalukdar007@gmail.com>
2024-05-04 04:49:48 +0530

    initial commit

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+// THIS ACTS AS HUB FOR ALL THE COMPONENTS. YOU CAN IMPORT THESE COMPONENTS AS IF THEY WERE SAVED IN THE SAME FILE
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore
+}
